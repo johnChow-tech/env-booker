d1d55a31bfc630f40c4e2d968b50d890adca0a2f
johnChow-tech <shulking@hotmail.com>
2026-02-17 17:21:25 +0900

    feat(settings.py): 分离SECRET_KEY到.env中，并重新发行

--- server/backend/settings.py
+++ server/backend/settings.py
@@ -10,8 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
+from dotenv import load_dotenv  # 新增
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ll^zu7a89ji%g5yz3tgrh7jo$dvcfqx1_1&h0&5w%+n!@ffbvk"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = []
 
